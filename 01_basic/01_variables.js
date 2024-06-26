d638d788fc7e3b5f8cf573f633e6978d250b91a3
Aman_Pandey <amanx609@gmail.com>
2024-06-26 12:51:21 +0000

    01_varaibales

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1223232
+let accountEmail = "amanx609@gmail.com"
+var accountPassword = "11223322"
+accountCity = "New Delhi"
+let accountState
+
+// accountId = 12
+accountEmail = "abc@gmail.com"
+accountPassword = "123"
+accountCity = "U.P"
+/*
+Prefer not to use var
+beacuse of issues in block scope and functional scope
+*/
+
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity,accountState])
